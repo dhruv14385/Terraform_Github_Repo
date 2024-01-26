3eeb490e85ff53f05aa973f00aae11840d2bcbc1
dhruv14385 <83332524+dhruv14385@users.noreply.github.com>
2024-01-27 02:31:47 +1100

    Update count.tf
    Removed ec2 resource

--- count/count.tf
+++ count/count.tf
@@ -1,16 +1,5 @@
 //This file will create 3 IAM users using count. 
 
-resource "aws_instance" "ec2_example" {
-
-   ami           = "ami-0a3c3a20c09d6f377"
-   instance_type =  "t2.micro"
-
-   tags = {
-        Name = "Terraform EC2"
-   }
-
-}
-
 resource "aws_iam_user" "example" {
   count = length(var.user_names)            //=3
   name  = var.user_names[count.index]
